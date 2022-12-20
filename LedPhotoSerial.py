b0431249ffdf29cfddcad45534c04f3ea477b6e0
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-12-20 22:13:44 +0800

    Update LedPhotoSerial.py

--- LedPhotoSerial.py
+++ LedPhotoSerial.py
@@ -3,10 +3,11 @@ import time
 
 lengths = {'f': 3,
            'u': 0,
-           'd': 0,
-           'a': 3, 
-           'b': 0}
-           
+           'd': 0
+           #,'a': 3, 
+           #'b': 0
+           }
+values = []           
 def get_connection(port): //подключение
     ser = serial.Serial(port, timeout = 1)
     return ser
@@ -26,32 +27,63 @@ val_max = 0
 val_min = 1024
 lst = []
 stop = False
+stinput = True 
 
 if __name__ == '__main__':
     ser = get_connection("/dev/cu.usbserial-1130") //подключаемся
     while True:
-          while stop: //чтобы пока не считал не работало
-            timeout = time.time() + 10 //текущее время +10с
-            cmd = 'f' 
-            val = send(ser, cmd.encode(), lengths[cmd]) // получаем значения
-            if val: //если зачение пришло
-                val = int(val) //записываем значение в инт
-                lst.append(val) //добавляем значение в массив??
-                if val > val_max:
-                    val_max = val //находим максимум
-                if val < val_min:
-                    val_min = val //находим минимум
-                if val < ((val_min + val_max) / 2):
-                    send(ser, 'u'.encode(), 0) //вкл
-                else:
-                    send(ser, 'd'.encode(), 0) //выкл
-                time.sleep(1)
-            if (time.time() > timeout): //если прошло 10с прерывается
-                break
-        inp = input("Enter command:") //считываем команду
-        lenght = lenghts.get(inp, 0) //записываем длину команды
-        send(ser, inp.encode(), lenght) //отправляем в ардуино
-        if inp == 'a':
-            stop = True
-        if inp == 'b':
-            stop = False
+        if stinput:
+            inp = int(input("Enter command:")) //считываем команду
+        if inp == 1:
+            send(ser, 'u'.encode(), 0) //вкл
+        if inp == 2:
+            send(ser, 'd'.encode(), 0) //выкл
+        if inp == 3:
+            while stop: //чтобы пока не считал не работало
+                timeout = time.time() + 10 //текущее время +10с
+                cmd = 'f' 
+                val = send(ser, cmd.encode(), lengths[cmd]) // получаем значения
+                if val: //если зачение пришло
+                    val = int(val) //записываем значение в инт
+                    lst.append(val) //добавляем значение в массив??
+                    if val > val_max:
+                        val_max = val //находим максимум
+                    if val < val_min:
+                        val_min = val //находим минимум
+                    if val < ((val_min + val_max) / 2):
+                        send(ser, 'u'.encode(), 0) //вкл
+                    else:
+                        send(ser, 'd'.encode(), 0) //выкл
+                    #time.sleep(1)
+                    if (time.time() > timeout): //если прошло 10с прерывается
+                        break
+        if inp == 4:
+            while stop: //чтобы пока не считал не работало
+                timeout = time.time() + 10 //текущее время +10с
+                cmd = 'f' 
+                val = send(ser, cmd.encode(), lengths[cmd]) // получаем значения
+                if val: //если зачение пришло
+                    val = int(val) //записываем значение в инт
+                    lst.append(val) //добавляем значение в массив??
+                    print(lst)
+                    if (time.time() > timeout): //если прошло 10с прерывается
+                        break
+        if inp == 5:
+            while stop: //чтобы пока не считал не работало
+                timeout = time.time() + 10 //текущее время +10с
+                cmd = 'f' 
+                val = send(ser, cmd.encode(), lengths[cmd]) // получаем значения
+                if val: //если зачение пришло
+                    val = int(val) //записываем значение в инт
+                    lst.append(val) //добавляем значение в массив??
+                    if val > val_max:
+                        val_max = val //находим максимум
+                    if val < val_min:
+                        val_min = val //находим минимум
+                    if val < ((val_min + val_max) / 2):
+                        send(ser, 'u'.encode(), 0) //вкл
+                    else:
+                        send(ser, 'd'.encode(), 0) //выкл
+                    time.sleep(1)
+                    if (time.time() > timeout): //если прошло 10с прерывается
+                        break
